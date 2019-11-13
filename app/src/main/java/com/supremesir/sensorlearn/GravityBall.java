73acd6f810a47b2e3607e9908c81e7bab4f30056
Haofan Fang <haofanfang@gmail.com>
2019-11-13 23:30:48 +0800

    update 全屏和横屏显示

--- app/src/main/java/com/supremesir/sensorlearn/GravityBall.java
+++ app/src/main/java/com/supremesir/sensorlearn/GravityBall.java
@@ -2,13 +2,49 @@ package com.supremesir.sensorlearn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+import javax.security.auth.callback.Callback;
 
 public class GravityBall extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 全屏显示窗口
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();//隐藏ActionBar标题栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //加载布局文件要放在setFlags之后
         setContentView(R.layout.activity_gravity_ball);
+
+        //强制横屏
+        if (getRequestedOrientation() != ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        super.onResume();
     }
+
+
+
+
 }
